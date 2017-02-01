d3f1cbb9394e89a4b68202c82e23102d9a12fb44
albert.denova <albert.denova@king.com>
2017-02-01 13:39:37 +0100

    Create new entity to add new items

---
+++ app/src/main/java/com/lasalle/second/part/medialibrary/views/activities/AddItem.java
@@ -0,0 +1,15 @@
+package com.lasalle.second.part.medialibrary.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lasalle.second.part.medialibrary.R;
+
+public class AddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
--- app/src/main/java/com/lasalle/second/part/medialibrary/views/activities/MainActivity.java
+++ app/src/main/java/com/lasalle/second/part/medialibrary/views/activities/MainActivity.java
@@ -1,15 +1,37 @@
 package com.lasalle.second.part.medialibrary.views.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.lasalle.second.part.medialibrary.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button addButton;
+    private Button listbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = (Button) findViewById(R.id.activity_main_add_button);
+        listbutton = (Button) findViewById(R.id.activity_main_list_button);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == addButton.getId())
+        {
+            Intent intent = new Intent(this, AddItem.class);
+            startActivity(intent);
+        }
+        else if(view.getId() == listbutton.getId())
+        {
+
+        }
     }
 }
