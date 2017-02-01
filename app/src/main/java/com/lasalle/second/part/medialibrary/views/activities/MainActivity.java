267b3c11d6dd8ab7689c630fbc960e1fcbcb5512
albert.denova <albert.denova@king.com>
2017-02-01 13:31:03 +0100

    Move activity to a more suitable place

--- app/src/main/java/com/lasalle/second/part/medialibrary/views/activities/MainActivity.java
+++ app/src/main/java/com/lasalle/second/part/medialibrary/views/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.lasalle.second.part.medialibrary;
+package com.lasalle.second.part.medialibrary.views.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lasalle.second.part.medialibrary.R;
 
 public class MainActivity extends AppCompatActivity {
 
