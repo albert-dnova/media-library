261740f5d8e1233c4ef26d29551bcfa4d702666c
albert.denova <albert.denova@king.com>
2017-02-09 14:57:40 +0100

    Enable strict mode

--- app/src/main/java/com/lasalle/second/part/medialibrary/MediaLibraryApplication.java
+++ app/src/main/java/com/lasalle/second/part/medialibrary/MediaLibraryApplication.java
@@ -1,6 +1,7 @@
 package com.lasalle.second.part.medialibrary;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.lasalle.second.part.medialibrary.data.impl.MovieDataFile;
 import com.lasalle.second.part.medialibrary.data.impl.MovieDataMock;
@@ -13,6 +14,21 @@ public class MediaLibraryApplication extends Application {
 
     @Override
     public void onCreate() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+
         super.onCreate();
 
         libraryManager = new LibraryManager(new MovieDataSharedPreferences(this));
